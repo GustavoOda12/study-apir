c3082123eadcace9e993cc1f3a5a3ff553e307fe
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-02 22:38:26 -0300

    code: linhas de codigo iniciais

---
+++ src/main/java/br/com/fiap/study_apir/PingController.java
@@ -0,0 +1,12 @@
+package br.com.fiap.study_apir;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+}
